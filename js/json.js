0ed0a797e59aa9e593d443e74984f11f8d51ae44
appstrategy <eprleads@gmail.com>
2013-05-24 10:05:48 -0700

    created inital json file with objects

---
+++ js/json.js
@@ -0,0 +1,21 @@
+// JSON Data to populate
+
+var json = {
+    "lead1": {
+        "name": ["Name:", "Peter"],
+        "phone": ["Phone:", "415-555-1212"],
+        "email": ["Email:", "peter@test.com"],
+        "date": ["Date:", "2013-05-15"],
+        "price": ["Price:", "250000"],
+        "info": ["Info:", "Test"]
+    },
+    "lead2": {
+        "name": ["Name:", "Ellie"],
+        "phone": ["Phone:", "415-555-1212"],
+        "email": ["Email:", "ellie@test.com"],
+        "date": ["Date:", "2013-05-15"],
+        "price": ["Price:", "300000"],
+        "info": ["Info:", "New One"]
+    }
+
+}
