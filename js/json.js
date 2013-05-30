c01a116b5b0088c27b73c6856df05d1b4f8912c8
appstrategy <eprleads@gmail.com>
2013-05-30 09:48:52 -0700

    updated images, this is functional

--- js/json.js
+++ js/json.js
@@ -1,6 +1,7 @@
 // JSON Data to populate
 
 var json = {
+
     "lead1": {
         "name": ["Name:", "Peter"],
         "phone": ["Phone:", "415-555-1212"],
@@ -8,9 +9,10 @@ var json = {
         "date": ["Date:", "2013-05-15"],
         "check": ["Checked:", "townhome"],
         "price": ["Price:", "250000"],
-        "bedrooms": ["Bedrooms:", "2+"],
+        "bedrooms": ["Bedrooms:", "3+"],
         "info": ["Info:", "Test"]
     },
+    
     "lead2": {
         "name": ["Name:", "Ellie"],
         "phone": ["Phone:", "415-555-1212"],
@@ -18,8 +20,20 @@ var json = {
         "date": ["Date:", "2013-05-15"],
         "check": ["Checked:", "Sfr"],
         "price": ["Price:", "300000"],
-        "bedrooms": ["Bedrooms:", "2+"],
+        "bedrooms": ["Bedrooms:", "1+"],
         "info": ["Info:", "New One"]
+    },
+    
+    "lead3": {
+        "name": ["Name:", "Ruby"],
+        "phone": ["Phone:", "415-555-1212"],
+        "email": ["Email:", "ruby@test.com"],
+        "date": ["Date:", "2013-06-18"],
+        "check": ["Checked:", "condo"],
+        "price": ["Price:", "350000"],
+        "bedrooms": ["Bedrooms:", "2+"],
+        "info": ["Info:", "Test"]
     }
+    
 
 }
